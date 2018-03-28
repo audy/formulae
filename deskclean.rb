a47c9dc1ce33c1de724dda609595f7078e8fabc3
Austin Davis-Richardson <harekrishna@gmail.com>
2018-03-27 22:25:13 -0700

    updates sha256

--- deskclean.rb
+++ deskclean.rb
@@ -1,15 +1,16 @@
 class Deskclean < Formula
-  homepage "https://github.com/audy/deskclean"
-  url "https://github.com/audy/deskclean/archive/0.1.0.tar.gz"
+  homepage 'https://github.com/audy/deskclean'
+  url 'https://github.com/audy/deskclean/archive/0.1.0.tar.gz'
+  sha256 'b976fab98e8e8ea96b4eda5b5eb6130511c917dd7980ee57517e70b4c73dd0b5'
 
-  depends_on "go" => :build
+  depends_on 'go' => :build
 
   def install
-    system "gobuild.sh"
-    bin.install ".gobuild/bin/deskclean" => "deskclean"
+    system 'gobuild.sh'
+    bin.install '.gobuild/bin/deskclean' => 'deskclean'
   end
 
   test do
-    system "#{bin}/deskclean", "--help"
+    system '#{bin}/deskclean', '--help'
   end
 end
